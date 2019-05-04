07f255a22591e4f1ab56257993e6dbb0c95fcd68
spencerwooo <spencerwoo98@foxmail.com>
2019-05-04 14:21:46 +0800

    Add: char_to_int and int_to_char

--- source.asm
+++ source.asm
@@ -7,20 +7,67 @@ printf PROTO C :ptr sbyte, :VARARG
 scanf PROTO C :ptr sbyte, :VARARG
 
 .data
+; Process input numbers in char
+input byte "%s", 0
 input_question_1 byte "Input the 1st number: ", 0
 input_question_2 byte "Input the 2nd number: ", 0
-input byte "%s", 0
+
+; Output final result in char
 output byte "%s * %s = %s", 0ah, 0
-num_1 byte 100 dup (0)
-num_2 byte 100 dup (0)
+
+; Big integers to calculate (Read in char)
+num_char_1 byte 100 dup (0)
+num_char_2 byte 100 dup (0)
+result_char byte 200 dup (0)
+
+; Each digit: "char to int"
+num_int_1 dword 100 dup(0)
+num_int_2 dword 100 dup(0)
+result_int dword 200 dup(0)
+
+; Length of numbers to multiply
+length_1 dword 0
+length_2 dword 0
+length_res dword 0
 
 .code
+char_to_int proc far C numChar:ptr byte, numInt:ptr byte, len:dword
+	mov ecx, len
+	mov esi, numChar
+	L1:
+		movzx eax, byte ptr [esi]
+		sub eax, 30H
+		push eax
+		inc esi
+		loop L1
+
+		mov ecx, len
+		mov esi, numInt
+	L2:
+		pop eax
+		mov dword ptr [esi], eax
+		add esi, 4
+		loop L2
+
+	ret
+char_to_int endp
+
+int_to_char proc far C uses eax esi ecx
+	mov ecx, length_res
+	mov esi, 0
+	
+	ret
+int_to_char endp
+
 main proc
 	invoke printf, offset input_question_1
-	invoke scanf, offset input, offset num_1
+	invoke scanf, offset input, offset num_char_1
 	invoke printf, offset input_question_2
-	invoke scanf, offset input, offset num_2
-	invoke printf, offset output, offset num_1, offset num_2, offset num_2
+	invoke scanf, offset input, offset num_char_2
+
+	
+
+	invoke printf, offset output, offset num_char_1, offset num_char_2, offset num_char_2
 	ret
 main endp
 end main
