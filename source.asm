c809111422a64d870c75abd97709f393001dc71f
spencerwooo <spencerwoo98@foxmail.com>
2019-05-04 13:01:38 +0800

    Add: input two numbers

--- source.asm
+++ source.asm
@@ -7,13 +7,20 @@ printf PROTO C :ptr sbyte, :VARARG
 scanf PROTO C :ptr sbyte, :VARARG
 
 .data
+input_question_1 byte "Input the 1st number: ", 0
+input_question_2 byte "Input the 2nd number: ", 0
 input byte "%s", 0
-output byte "The number is: %s", 0ah, 0
-num byte 65536 dup (?)
+output byte "%s * %s = %s", 0ah, 0
+num_1 byte 100 dup (0)
+num_2 byte 100 dup (0)
 
 .code
-main:
-	invoke scanf, offset input, offset num
-	invoke printf, offset output, offset num
+main proc
+	invoke printf, offset input_question_1
+	invoke scanf, offset input, offset num_1
+	invoke printf, offset input_question_2
+	invoke scanf, offset input, offset num_2
+	invoke printf, offset output, offset num_1, offset num_2, offset num_2
 	ret
+main endp
 end main
